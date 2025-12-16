9516cf1a0fb0b8d580ed439bfb5a1dfa193d1b61
ali.qm <ali.qm@erp-intel.com>
2025-12-16 22:38:22 +0400

    fixed

--- src/pages/job-detail/job-detail-three.jsx
+++ src/pages/job-detail/job-detail-three.jsx
@@ -219,7 +219,6 @@ export default function JobDetailThree() {
                   ))}
                 </ul>
               )}
-
               {role === "candidate" && <div className="mt-5">
                 <Link
                   to="#"
